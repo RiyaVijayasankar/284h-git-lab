042da20bdd77e9241502f556e42866229cb758c7
RiyaVijayasankar <rvijayasankar2@huskers.unl.edu>
2026-01-21 15:11:38 -0600

    Add add_numbers function and test.

--- test_git_lab.py
+++ test_git_lab.py
@@ -5,8 +5,14 @@ from git_lab import (
     is_palindrome,
     calculate_triangle_area,
     is_prime,
+    add_numbers,
 )
 import math
+
+class TestGitLab(unittest.TestCase):
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
 
 class TestUtils(unittest.TestCase):
 
